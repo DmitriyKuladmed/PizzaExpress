9294b8eefd9af60977c896a1bc0d0a651244a9ed
dimak <dima.kulaga5@gmail.com>
2023-11-02 20:25:17 +0300

    created a connection to the database. changed models for many-to-many relationships

--- backend/PExpress/models.py
+++ backend/PExpress/models.py
@@ -4,11 +4,11 @@ from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseU
 
 class User(AbstractBaseUser):
     id = models.BigAutoField(primary_key=True)
-    nickname = models.CharField(max_length=25)
-    tg_nickname = models.CharField(max_length=50)
+    nickname = models.CharField(max_length=25, unique=True)
+    tg_nickname = models.CharField(max_length=50, null=True)
 
     USERNAME_FIELD = 'nickname'
-    REQUIRED_FIELDS = ['id', 'nickname', 'tg_nickname']
+    REQUIRED_FIELDS = ['id', 'tg_nickname']
 
     def get_nickname(self):
         return self.nickname
@@ -16,19 +16,15 @@ class User(AbstractBaseUser):
 
 class Order(models.Model):
     user_id = models.ForeignKey(User, on_delete=models.CASCADE)
-    courier = models.CharField(max_length=60)
-    status = models.CharField(max_length=60)
+    courier = models.CharField(max_length=60, default=None, null=True)
+    status = models.CharField(max_length=60, default="Подтвердите заказ")
 
     def get_status(self):
         return self.status
 
 
-class DishForOrder(models.Model):
-    order_id = models.ForeignKey(Order, on_delete=models.CASCADE)
-    dish_id = models.IntegerField
-
-
 class Dish(models.Model):
+    id = models.AutoField(primary_key=True)
     pizza_name = models.CharField(max_length=100)
     cooking_time = models.IntegerField
     gram = models.IntegerField
@@ -38,21 +34,21 @@ class Dish(models.Model):
         return self.pizza_name
 
 
-class DishElement(models.Model):
+class DishForOrder(models.Model):
+    order_id = models.ForeignKey(Order, on_delete=models.CASCADE)
     dish_id = models.ForeignKey(Dish, on_delete=models.CASCADE)
-    ingredient_id = models.IntegerField
 
 
-class Ingredient(models):
+class Ingredient(models.Model):
     ingredient_name = models.CharField(max_length=60)
 
     def get_ingredient_name(self):
         return self.ingredient_name
 
 
-class MenuElement(models.Model):
+class DishElement(models.Model):
     dish_id = models.ForeignKey(Dish, on_delete=models.CASCADE)
-    menu_id = models.IntegerField
+    ingredient_id = models.ForeignKey(Ingredient, on_delete=models.CASCADE)
 
 
 class Menu(models.Model):
@@ -62,3 +58,11 @@ class Menu(models.Model):
         return self.menu_name
 
 
+class MenuElement(models.Model):
+    dish_id = models.ForeignKey(Dish, on_delete=models.CASCADE)
+    menu_id = models.ForeignKey(Menu, on_delete=models.CASCADE)
+
+
+class AdditionalIngredient(models.Model):
+    dish_for_order_id = models.ForeignKey(Dish, on_delete=models.CASCADE)
+    ingredient = models.ForeignKey(Ingredient, on_delete=models.CASCADE)
--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -79,8 +79,12 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'PizzaExpress',
+        'USER': 'postgres',
+        'PASSWORD': 'admin2004',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
